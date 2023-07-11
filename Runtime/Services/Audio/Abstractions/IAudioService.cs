27ef9f984ddf35cb056947e2e1acf13913e87293
Siyuan Wu <wusiyuan.wsy@bytedance.com>
2023-07-11 21:17:07 +0800

    🔧 fix(JamForge.Runtime.Player.csproj.DotSettings): remove unused code inspection settings for audio services and store implementations
    🔧 fix(JamForge.Runtime.csproj.DotSettings): remove unused code inspection settings for audio services and store implementations
    📦 chore: add new audio-related files and folders to the project

---
+++ Runtime/Services/Audio/Abstractions/IAudioService.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace JamForge.Audio
+{
+    public interface IAudioHandle
+    {
+        bool IsPlaying { get; }
+
+        void Stop();
+
+        void Pause();
+
+        void Resume();
+    }
+
+    public interface IGetAudioClip
+    {
+        AudioClip GetClip();
+    }
+
+    public interface IAudioBase
+    {
+        float Volume { get; }
+
+        float Pitch { get; }
+    }
+
+    public interface IAudioMusic : IAudioBase, IGetAudioClip { }
+
+    public interface IAudioSoundEffect : IAudioBase, IGetAudioClip { }
+
+    public interface IAudioService : IAudioBase
+    {
+        public void Play(IAudioSoundEffect sfx);
+
+        public void Play(IAudioSoundEffect sfx, Vector3 position);
+
+        public IAudioHandle Play(IAudioMusic music, string channel = null);
+
+        public IAudioHandle CrossFade(IAudioMusic music, string channel = null);
+
+        public void SetGlobalVolume(float volume);
+
+        public void SetGlobalPitch(float pitch);
+
+        public void StopAll();
+
+        public void PauseAll();
+
+        public void ResumeAll();
+    }
+}
