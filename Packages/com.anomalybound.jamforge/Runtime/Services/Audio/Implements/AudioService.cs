d4ca1cf0356af871839f1d258ae6a72fc6b83eff
Siyuan Wu <wusiyuan.wsy@bytedance.com>
2023-07-11 21:17:07 +0800

    🔧 fix(JamForge.Runtime.Player.csproj.DotSettings): remove unused code inspection settings for audio services and store implementations
    🔧 fix(JamForge.Runtime.csproj.DotSettings): remove unused code inspection settings for audio services and store implementations
    📦 chore: add new audio-related files and folders to the project

---
+++ Packages/com.anomalybound.jamforge/Runtime/Services/Audio/Implements/AudioService.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace JamForge.Audio
+{
+    public class AudioHandle : IAudioHandle
+    {
+        public bool IsPlaying { get; set; }
+
+        public AudioSource AudioSource { get; }
+
+        public AudioHandle(AudioSource audioSource)
+        {
+            AudioSource = audioSource;
+        }
+
+        public void Stop() { }
+
+        public void Pause() { }
+
+        public void Resume() { }
+    }
+
+    public class AudioService : IAudioService
+    {
+        public float Volume { get; private set; }
+
+        public float Pitch { get; private set; }
+
+        private List<AudioHandle> Handles { get; } = new();
+
+        private AudioSource _sfxSource;
+        private readonly Dictionary<string, AudioSource> _musicSource = new();
+
+        private readonly Transform _root;
+        private const string DefaultMusicChannel = "GeneralMusic";
+
+        private AudioSource CreateAudioSource(string name)
+        {
+            var audioSource = new GameObject(name).AddComponent<AudioSource>();
+            audioSource.transform.SetParent(_root, false);
+            return audioSource;
+        }
+
+        public AudioService()
+        {
+            _root = new GameObject("JamForge AudioService").transform;
+            _sfxSource = CreateAudioSource("SoundEffects");
+
+            var musicSource = CreateAudioSource(DefaultMusicChannel);
+            _musicSource.TryAdd(DefaultMusicChannel, musicSource);
+        }
+
+        public void Play(IAudioSoundEffect sfx) { }
+
+        public void Play(IAudioSoundEffect sfx, Vector3 position) { }
+
+        public IAudioHandle Play(IAudioMusic music, string channel = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IAudioHandle CrossFade(IAudioMusic music, string channel = null)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetGlobalVolume(float volume)
+        {
+            Volume = volume;
+        }
+
+        public void SetGlobalPitch(float pitch)
+        {
+            Pitch = pitch;
+        }
+
+        public void StopAll()
+        {
+            for (var i = 0; i < Handles.Count; i++)
+            {
+                Handles[i].Stop();
+            }
+        }
+
+        public void PauseAll()
+        {
+            for (var i = 0; i < Handles.Count; i++)
+            {
+                Handles[i].Pause();
+            }
+        }
+
+        public void ResumeAll()
+        {
+            for (var i = 0; i < Handles.Count; i++)
+            {
+                Handles[i].Resume();
+            }
+        }
+    }
+}
